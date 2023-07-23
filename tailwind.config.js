9fce88c5fbc873e592dc26f7443cf5833cc674ec
sumonali940 <isumon105@gmail.com>
2023-07-23 13:22:45 +0600

    This is my first comment header section

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  module.exports = {
+    //...
+    daisyui: {
+      themes: ["light", "dark", "cupcake"],
+    },
+  }
+  module.exports = {
+    theme: {
+      colors: {
+        btBtn: 'rgba(231, 111, 81, 0.10)',
+        bgColor: '#FDF1EE',
+      }
+    }
+  }
